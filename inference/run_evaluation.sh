28ba5196132604dda68ad469697538d7443a2fc8
KJ12124 <kwon04210@naver.com>
2025-08-25 15:02:33 +0900

    move

--- inference/run_evaluation.sh
+++ inference/run_evaluation.sh
@@ -1,8 +1,8 @@
 TEST_DATA=(dl19 dl20)
 for data in ${TEST_DATA[@]}; do
-    CUDA_VISIBLE_DEVICES=0 python3 evaluation.py --input_path eval_data/${data}.jsonl \
+    CUDA_VISIBLE_DEVICES=0 python3 evaluation.py --input_path /home/tako/kjun/MVT5_inference/eval_data/baseline/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
         --n_special_tokens 4 \
-        --model_path /data/kjun/checkpoints/MVT5_RDLLM/MVP_REPRODUCE_CONDA_CHECK/tfmr_0_step13000 \
+        --model_path /data/kjun/checkpoints/EMNLP_FINAL_CHECKPOINT/base \
         --measure_flops
 done
