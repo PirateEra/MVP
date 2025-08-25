a3d83068e0510ba31c732a25f47e9a23904289d8
KJ12124 <kwon04210@naver.com>
2025-08-25 15:04:07 +0900

    change shell file

--- inference/run_evaluation.sh
+++ inference/run_evaluation.sh
@@ -1,8 +1,7 @@
 TEST_DATA=(dl19 dl20)
 for data in ${TEST_DATA[@]}; do
-    CUDA_VISIBLE_DEVICES=0 python3 evaluation.py --input_path /home/tako/kjun/MVT5_inference/eval_data/baseline/${data}.jsonl \
+    CUDA_VISIBLE_DEVICES=0 python3 evaluation.py --input_path ../eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
         --n_special_tokens 4 \
-        --model_path /data/kjun/checkpoints/EMNLP_FINAL_CHECKPOINT/base \
-        --measure_flops
+        --model_path /MODEL/PATH
 done
