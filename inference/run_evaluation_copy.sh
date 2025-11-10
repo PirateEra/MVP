3c1a05ac9fb77a37089ac22114b8081d5100536d
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-11 00:28:52 +0100

    test new datasets position bias table

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -1,6 +1,8 @@
 # TEST_DATA=(dl19 dl20 trec-covid nfcorpus signal news robust04 scifact touche dbpedia-entity)
+TEST_DATA=(dl19 dl19_rev dl19_shuffle dl20 dl20_rev dl20_shuffle news news_rev news_shuffle)
 # TEST_DATA=(dl19 dl20)
-TEST_DATA=(news)
+# deze krijgen een error op snellius
+# TEST_DATA=(news)
 # TEST_DATA=(robust04)
 # TEST_DATA=(touche)
 # additional flags
@@ -11,5 +13,5 @@ for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
         --n_special_tokens 4 \
-        --model_path Jun421/MVP-3b
+        --model_path Jun421/MVP-base
 done
