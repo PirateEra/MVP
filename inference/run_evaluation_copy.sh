818b6e817883fb7e71b6c51381cfdf650eb4c80d
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-10 17:19:27 +0100

    test

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -3,5 +3,6 @@ for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
         --n_special_tokens 4 \
-        --model_path Jun421/MVP-3b
+        --model_path Jun421/MVP-base
+        # --model_path Jun421/MVP-3b
 done
