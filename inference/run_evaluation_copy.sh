9e9f17586c47b8915725c20e3f56ff2bcf7f4e23
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-10 17:29:45 +0100

    all data included test

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -1,4 +1,4 @@
-TEST_DATA=(dl19 dl20)
+TEST_DATA=(dl19 dl20 trec-covid nfcorpus signal news robust04 scifact touche dbpedia-entity)
 for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
