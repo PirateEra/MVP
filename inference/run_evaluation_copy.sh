ac6043e7b45e8181473e90329094dca88d79cbd8
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-10 18:07:45 +0100

    test other datasets

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -1,10 +1,11 @@
-# TEST_DATA=(dl19 dl20 trec-covid nfcorpus signal news robust04 scifact touche dbpedia-entity)
-TEST_DATA=(dl19 dl20)
+TEST_DATA=(dl19 dl20 trec-covid nfcorpus signal news robust04 scifact touche dbpedia-entity)
+# TEST_DATA=(dl19 dl20)
+# additional flags
+# --model_path Jun421/MVP-3b
+# --measure_flops 
 for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
         --n_special_tokens 4 \
-        --measure_flops \
         --model_path Jun421/MVP-base
-        # --model_path Jun421/MVP-3b
 done
