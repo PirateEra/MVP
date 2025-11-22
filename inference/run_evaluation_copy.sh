92d73f996aaf26e7b355ec8df7bab5a4ddd135f9
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-23 00:56:28 +0100

    .

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -15,5 +15,5 @@ for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
         --n_special_tokens 4 \
-        --model_path ../checkpoints/MVP/tfmr_0_step25000
+        --model_path ../checkpoints_1views/MVP/tfmr_0_step16000
 done
