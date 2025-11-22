c0a75f18f1de2db5439e2c02715ee0188b8c6d5a
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-23 00:56:07 +0100

    1 view eval

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -10,6 +10,7 @@ TEST_DATA=(dl19 dl20 trec-covid nfcorpus signal news robust04 scifact touche dbp
 # --model_path Jun421/MVP-base
 # --measure_flops 
 # --model_path ../checkpoints/MVP/tfmr_0_step25000
+# --model_path ../checkpoints_1views/MVP/tfmr_0_step16000
 for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
