dd1bacbbda3fafc66a5ac378528b8d4382669721
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-23 16:36:10 +0100

    3 views

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -18,6 +18,6 @@ TEST_DATA=(dl19 dl20 trec-covid nfcorpus signal news robust04 scifact touche dbp
 for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
-        --n_special_tokens 5 \
-        --model_path ../checkpoints_5views/MVP/tfmr_0_step17000
+        --n_special_tokens 3 \
+        --model_path ../checkpoints_3views/MVP/tfmr_0_step24000
 done
