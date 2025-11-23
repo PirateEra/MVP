b4187be1e34bbdd5f5934d30cec9617d1e788293
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-23 18:49:27 +0100

    .

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -1,10 +1,6 @@
 TEST_DATA=(dl19 dl20 trec-covid nfcorpus signal news robust04 scifact touche dbpedia-entity)
 # TEST_DATA=(dl19 dl19_rev dl19_shuffle dl20 dl20_rev dl20_shuffle news news_rev news_shuffle)
 # TEST_DATA=(dl19 dl20)
-# deze krijgen een error op snellius
-# TEST_DATA=(news)
-# TEST_DATA=(robust04)
-# TEST_DATA=(touche)
 # additional flags
 # --model_path Jun421/MVP-3b
 # --model_path Jun421/MVP-base
