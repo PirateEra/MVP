f57cd1a8eda838a3f4b497513f425a10f7c31b55
Xinyu Zhu <xyz.hm@hotmail.com>
2025-11-23 01:08:57 +0100

    eval 5 views

--- inference/run_evaluation_copy.sh
+++ inference/run_evaluation_copy.sh
@@ -19,5 +19,5 @@ for data in ${TEST_DATA[@]}; do
     CUDA_VISIBLE_DEVICES=0 python3 ./evaluation.py --input_path ./eval_data/${data}.jsonl \
         --output_path ./outputs/mvp-${data}.jsonl --topk 100 \
         --n_special_tokens 4 \
-        --model_path ../checkpoints_3views/MVP/tfmr_0_step24000
+        --model_path ../checkpoints_5views/MVP/tfmr_0_step17000
 done
