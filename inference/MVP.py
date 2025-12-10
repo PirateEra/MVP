d5cfb7ffe08986d8a46441f48ef52b2063923029
Xinyu Zhu <xyz.hm@hotmail.com>
2025-12-11 00:48:12 +0100

    test diff weighting 1

--- inference/MVP.py
+++ inference/MVP.py
@@ -148,17 +148,22 @@ class MVP(transformers.T5ForConditionalGeneration):
 
         # weights = [x/sum(beir_avg) for x in beir_avg]
         # weight_tensor = torch.tensor(weights).to(logits.device)
-        # # test 4-view model
-        # # weight_tensor = torch.tensor([0.30, 0.15, 0.15, 0.40]).to(logits.device)
-        # # weight_tensor = torch.tensor([0.15, 0.15, 0.30, 0.40]).to(logits.device)
-        # repeat_weight_tensor = weight_tensor.repeat(self.n_passages, 1)
-        # transposed = repeat_weight_tensor.T
-        # repeat_over_batchsize = transposed.unsqueeze(0).repeat(bsz, 1, 1)
-        # weight_multiplication = torch.mul(logits, repeat_over_batchsize)
-        # logits = torch.sum(weight_multiplication, dim=1)
+
+        # test 4-view model
+        # weight_tensor = torch.tensor([0.30, 0.15, 0.15, 0.40]).to(logits.device)
+        # weight_tensor = torch.tensor([0.15, 0.15, 0.30, 0.40]).to(logits.device)
+        # test 6-view model
+        weight_tensor = torch.tensor([0.25, 0.10, 0.10, 0.10, 0.10, 0.35]).to(logits.device)
+        # weight_tensor = torch.tensor([0.10, 0.10, 0.10, 0.10, 0.25, 0.35]).to(logits.device)
+
+        repeat_weight_tensor = weight_tensor.repeat(self.n_passages, 1)
+        transposed = repeat_weight_tensor.T
+        repeat_over_batchsize = transposed.unsqueeze(0).repeat(bsz, 1, 1)
+        weight_multiplication = torch.mul(logits, repeat_over_batchsize)
+        logits = torch.sum(weight_multiplication, dim=1)
 
         # their original mean
-        logits = logits.mean(dim=1)
+        # logits = logits.mean(dim=1)
 
         loss = 0
         if labels is not None:
