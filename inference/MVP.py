082f223c8e07e7b38e1a8f775733bd473089417d
Xinyu Zhu <xyz.hm@hotmail.com>
2025-12-10 18:21:04 +0100

    view 4 - 4view model

--- inference/MVP.py
+++ inference/MVP.py
@@ -133,9 +133,9 @@ class MVP(transformers.T5ForConditionalGeneration):
         # only view 2: 
         # logits = logits[:, 1:2, :].squeeze(1)
         # only view 3: 
-        logits = logits[:, 2:3, :].squeeze(1)
+        # logits = logits[:, 2:3, :].squeeze(1)
         # only view 4: 
-        # logits = logits[:, 3:4, :].squeeze(1)
+        logits = logits[:, 3:4, :].squeeze(1)
         # only view 5: 
         # logits = logits[:, 4:5, :].squeeze(1)
         # only view 6: 
